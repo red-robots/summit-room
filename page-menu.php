76123d3cb95b131823664e723895eab6c0c0a375
Austin Crane <austincrane@gmail.com>
2016-10-25 15:42:51 -0400

    push request down

--- page-menu.php
+++ page-menu.php
@@ -25,7 +25,7 @@ get_header(); ?>
 
 				?>
 
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<article id="post-<?php the_ID(); ?>" class="menu-article">
 					<header class="entry-header">
 						<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 					</header><!-- .entry-header -->
